c297727de064de382e2c6aa5bd5a70fb9431a41b
JP-Pardinho <jppardinhor@gmail.com>
2025-11-24 20:28:56 -0300

    feat(Proteger a Área de Gestão): Implementação do controler, rotas, tela index de usuarios, validação de acesso a tela de usuarios

--- src/Controller/Usuario/Listar/Controller.php
+++ src/Controller/Usuario/Listar/Controller.php
@@ -6,6 +6,7 @@ use App\Repository\UsuarioRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class Controller extends AbstractController
 {   
@@ -19,6 +20,13 @@ final class Controller extends AbstractController
     #[Route('/usuarios', name: self::ROUTE_NAME, methods:'GET')]
     public function show(): Response
     {
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            $this->addFlash('danger', 'Acesso negado. Área restrita a administradores.');
+            return $this->render('erro/erro.html.twig', [
+                'erro' => '401'
+            ]);
+        }
+
         return $this->render('usuario/index.html.twig', [
             'usuarios' => $this->usuarioRepository->findAll()
         ]);
